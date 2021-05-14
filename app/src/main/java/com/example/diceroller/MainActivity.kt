7dc8a66356131dfe1e54a62941a4c8f32fd3db66
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-05-14 20:39:46 +0700

    refactor: add a click handler to display a toast

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,11 @@ class MainActivity : AppCompatActivity() {
 
         // Get a reference to the 'roll_button' button
         val rollButton: Button = findViewById(R.id.roll_button)
+    }
+
+    // Add a click handler to display a toast
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked !",
+            Toast.LENGTH_SHORT).show()
     }
 }
